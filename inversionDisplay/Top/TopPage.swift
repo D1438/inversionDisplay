140e2fc640e5b037596405fe345279bfb15a290f
I.D <daisuke.workin@gmail.com>
2020-08-12 21:56:58 +0900

    Initialize

---
+++ inversionDisplay/Top/TopPage.swift
@@ -0,0 +1,23 @@
+//
+//  TopPage.swift
+//  inversionDisplay
+//
+//  Created by 石澤大輔 on 2020/08/12.
+//  Copyright © 2020 石澤大輔. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TopPageViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func DebugButton(_ sender: Any) {
+        print("押せたよ！")
+    }
+    
+}
---
+++ inversionDisplay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  inversionDisplay
+//
+//  Created by 石澤大輔 on 2020/08/06.
+//  Copyright © 2020 石澤大輔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
